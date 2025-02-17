19f1a1de405bfad2f0e016970bde650e3e0bb2de
riccardo <scaringi_riccardo@libero.it>
2025-02-17 10:51:18 +0100

    Create CostComparison.js

---
+++ frontend/src/CostComparison.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+
+const CostComparison = () => {
+  return (
+    <Paper>
+      <Typography variant="h6">Cost Comparison Coming Soon</Typography>
+    </Paper>
+  );
+};
+
+export default CostComparison;
